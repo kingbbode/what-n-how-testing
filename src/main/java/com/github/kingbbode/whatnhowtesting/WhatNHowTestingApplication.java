f80fa8cf3d8b6a126545159b61337c1f3d1a79d4
kingbbode <kingbbode@gmail.com>
2019-04-07 17:51:32 +0900

    init

---
+++ src/main/java/com/github/kingbbode/whatnhowtesting/WhatNHowTestingApplication.java
@@ -0,0 +1,12 @@
+package com.github.kingbbode.whatnhowtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatNHowTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatNHowTestingApplication.class, args);
+    }
+}
